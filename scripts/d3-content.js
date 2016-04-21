1213be8e2d7ac80b3028ff15453d1ac8b3809689
Jake Hartz <jhartz@outlook.com>
2016-04-21 00:11:21 -0400

    Moved graph structure to separate file; improved D3 graph rendering

--- scripts/d3-content.js
+++ scripts/d3-content.js
@@ -1,34 +1,5 @@
-var MY_TEST_DATA = {
-    "kitchen": {
-        title: "Kitchen",
-        children: ["kitchen2", "stovetop", "bathroom"],
-        data: {
-            latitude: 5,
-            longitude: 20
-        }
-    },
-    "kitchen2": {
-        title: "Kitchen 2",
-        children: ["stovetop"]
-    },
-    "stovetop": {
-        title: "Stovetop"
-    },
-    "bathroom": {
-        title: "Bathroom"
-    },
-    "store": {
-        title: "Store",
-        children: ["bathroom"]
-    }
-};
-
-
-
+// Anonymous function to encapsulate D3 initialization
 (function () {
-    var root = MY_TEST_DATA;
-    var nodes = getNodes(root);
-
     // The main D3 Force layout
     var force = d3.layout.force()
         .charge(-500)
@@ -46,40 +17,6 @@ var MY_TEST_DATA = {
 
 
     /**
-     * Class representing a node in our graph.
-     * @constructor
-     */
-    function Node(key, obj, nodesByKey) {
-        this.id = this.key = key;
-        this.title = obj.title;
-        this.data = obj.data;
-        this.childrenKeys = obj.children || [];
-
-        this.nodesByKey = nodesByKey;
-        this.childrenVisible = true;
-    }
-
-    /**
-     * Get link objects for each child of this Node.
-     */
-    Node.prototype.getChildLinks = function () {
-        if (!this.childrenVisible) {
-            return [];
-        }
-
-        var sourceNode = this,
-            nodesByKey = this.nodesByKey;
-
-        return this.childrenKeys.map(function (childKey) {
-            return {
-                source: sourceNode,
-                target: nodesByKey[childKey]
-            };
-        });
-    };
-
-
-    /**
      * Handle resizing of the force layout to fit its parent size.
      */
     function resize() {
@@ -90,39 +27,12 @@ var MY_TEST_DATA = {
 
 
     /**
-     * Transform a tree structure of nodes into an array of Node objects.
-     */
-    function getNodes(root) {
-        var nodes = [];
-        var nodesByKey = {};
-
-        var key, obj, node;
-        for (key in root) if (root.hasOwnProperty(key)) {
-            obj = root[key];
-            node = new Node(key, obj, nodesByKey);
-            nodesByKey[key] = node;
-            nodes.push(node);
-        }
-
-        return nodes;
-    }
-
-
-    /**
-     * Create an array of links between nodes from an array of Node objects.
-     */
-    function getLinks(nodes) {
-        return nodes.reduce(function (prev, node) {
-            return prev.concat(node.getChildLinks());
-        }, []);
-    }
-
-
-    /**
      * Update the D3 visualization.
      */
     function update() {
-        var links = getLinks(nodes);
+        var graphData = graph.get();
+        var nodes = graphData.nodes,
+            links = graphData.links;
 
         // Restart the force layout
         force
@@ -138,7 +48,7 @@ var MY_TEST_DATA = {
 
         // Enter any new links
         svgLinks.enter()
-            .append("line")
+            .insert("line", ".node")
             .attr("class", "link")
             .attr("x1", function (d) { return d.source.x; })
             .attr("y1", function (d) { return d.source.y; })
@@ -161,12 +71,14 @@ var MY_TEST_DATA = {
             .call(force.drag);
 
         g.append("circle")
-            .attr("r", 35)
-            .style("fill", color);
+            .attr("r", 35);
 
         g.append("text")
             .style("text-anchor", "middle")
             .text(function (d) { return d.title; });
+
+        // Update all nodes
+        svgNodes.select("circle").style("fill", color);
     }
 
     function tick() {
@@ -184,11 +96,14 @@ var MY_TEST_DATA = {
 
     // Color leaf nodes orange, and packages white or blue
     function color(d) {
-        if (d.childrenKeys.length == 0) {
+        if (d.isRoot) {
+            // Root node
             return "#fd8d3c";
-        } else if (d.childrenVisible) {
+        } else if (d.childrenVisible || d.childrenKeys.length == 0) {
+            // Visible children or no children
             return "#c6dbef";
         } else {
+            // Hidden children
             return "#3182bd";
         }
     }
@@ -203,14 +118,13 @@ var MY_TEST_DATA = {
 
 
     window.addEventListener("load", function () {
-        // Start
+        graph.init(MY_TEST_DATA);
         resize();
         update();
     }, false);
 
     window.addEventListener("resize", function () {
         resize();
-        update();
     }, false);
 })();
 
---
+++ scripts/graph.js
@@ -0,0 +1,133 @@
+// Object to hold all exported functions from this file
+var graph = {};
+
+// Anonymous function to encapsulate private data and functions
+(function () {
+    // The nodes that we have created
+    var _nodesByKey = {};
+    var _nodes = [];
+    var _rootKeys = [];
+
+    /**
+     * Class representing a node in our graph.
+     *
+     * @constructor
+     */
+    function Node(key, obj) {
+        this.id = this.key = key;
+        this.isRoot = !!obj.root;
+        this.title = obj.title;
+        this.data = obj.data;
+        this.childrenKeys = obj.children || [];
+        this.childrenVisible = true;
+    }
+
+
+    /**
+     * Transform a tree structure of nodes into Node objects.
+     *
+     * @public
+     */
+    graph.init = function (data) {
+        // Reset the nodes
+        _nodesByKey = {};
+        _nodes = [];
+        _rootKeys = [];
+
+        var key, obj, node;
+        for (key in data) if (data.hasOwnProperty(key)) {
+            obj = data[key];
+            node = new Node(key, obj);
+
+            _nodesByKey[key] = node;
+            _nodes.push(node);
+            if (node.isRoot) _rootKeys.push(key);
+        }
+    }
+
+
+    /**
+     * Get the nodes and the links between nodes in the graph.
+     *
+     * @public
+     */
+    graph.get = function () {
+        // Only return nodes reachable from a root node
+        var visitedKeys = {};
+        var nodes = [];
+        var links = [];
+
+        var queue = _rootKeys.slice();
+        var currentKey, currentNode;
+        while (queue.length > 0) {
+            // Get the node at the front of the queue
+            currentKey = queue.shift();
+            currentNode = _nodesByKey[currentKey];
+
+            if (!currentNode) {
+                // AHH! Found a key that doesn't correspond to a node!
+                throw new Error("Key \"" + currentKey + "\" does not " +
+                        "correspond to an existing node!");
+            }
+
+            // Add it to our list, and mark it as visited
+            visitedKeys[currentKey] = true;
+            nodes.push(currentNode);
+
+            // Look at this node's children, if they're visible
+            if (currentNode.childrenVisible) {
+                currentNode.childrenKeys.forEach(function (childKey) {
+                    // Add the parent-child link to the list
+                    links.push({
+                        source: currentNode,
+                        target: _nodesByKey[childKey]
+                    });
+
+                    // If we haven't visited this child, add it to the queue
+                    if (!visitedKeys[childKey]) {
+                        queue.push(childKey);
+                        visitedKeys[childKey] = true;
+                    }
+                });
+            }
+        }
+
+        return {
+            nodes: nodes,
+            links: links
+        };
+    };
+
+
+    /**
+     * Check whether we can reach another Node from this Node through visible
+     * children.
+     *
+     * @public
+     */
+    Node.prototype.canReachNode = function (otherNode) {
+        // BFS search from this node to the other node
+        var queue = [this];
+        var visited = {};
+
+        var current, child, i;
+        while (queue.length > 0) {
+            current = queue.shift();
+            if (current == otherNode) {
+                return true;
+            }
+            if (otherNode.childrenVisible) {
+                for (i = 0; i < otherNode.childrenKeys.length; i++) {
+                    child = _nodesByKey[otherNode.childrenKeys[i]];
+                    if (!visited[child]) {
+                        queue.push(child);
+                        visited[child] = true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    };
+})();
+
